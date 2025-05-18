16a1bf2c08f734c700cd75e8b56aa5a075721a90
Nicollas <nicollas225@gmail.com>
2025-05-18 11:59:51 -0300

    Alterações de funcionalidades
    Verificação no login, e alinhamento de divs

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Função para simular a verificação de login
-function verificarLogin() {
-    const nome = document.getElementById('nome').value;
-    const senha = document.getElementById('senha').value;
+document.getElementById('login').addEventListener('submit',function(event) {
+    event.preventDefault(); // Impede o envio tradicional (recarregar página)
+    
+    const nome = document.getElementById('username').value;
+    const senha = document.getElementById('password').value;
 
     // Simulação de verificação de dados para um futuro banco de dados
-    if (nome === 'admin' && senha === '1234') {
+    if (nome === '123' && senha === '123') {
         alert('Login bem-sucedido!');
         window.location.href = 'pages/alltasks.html';
     } else {
         alert('Nome ou senha incorretos. Tente novamente.');
     }
-}
-
-document.querySelector('.login-button').addEventListener('click', verificarLogin);
+});
